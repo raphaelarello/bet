954f016cd92d00947a8d872b19bb0cb7a7c7662f
Raphael Arello <raphael.arello@consulth.com.br>
2026-03-20 21:10:35 -0400

    Add missing leagues.ts file

---
+++ client/src/lib/leagues.ts
@@ -0,0 +1,12 @@
+export const FEATURED_LEAGUES = [
+  { id: '3918', name: 'Premier League', country: 'England' },
+  { id: '740', name: 'La Liga', country: 'Spain' },
+  { id: '720', name: 'Bundesliga', country: 'Germany' },
+  { id: '730', name: 'Serie A', country: 'Italy' },
+  { id: '710', name: 'Ligue 1', country: 'France' },
+  { id: '4351', name: 'Brasileirão Série A', country: 'Brazil' },
+  { id: '23', name: 'Champions League', country: 'Europe' },
+  { id: '2', name: 'Europa League', country: 'Europe' },
+  { id: '21', name: 'Libertadores', country: 'South America' },
+  { id: '22', name: 'Sudamericana', country: 'South America' },
+];
